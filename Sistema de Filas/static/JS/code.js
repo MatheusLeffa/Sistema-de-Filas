2342ea378fb631ad1da076886c7e9d82acbdfefe
Matt_Hilbert <83082324+MatheusLeffa@users.noreply.github.com>
2021-11-24 18:57:10 -0300

    update

--- Sistema de Filas/static/JS/code.js
+++ Sistema de Filas/static/JS/code.js
@@ -1,89 +1,89 @@
 // Queue CLASS
 class Queue {
-  constructor() {
-    this.count = 0;
-    this.lowestCount = 0;
-    this.elements = {};
-  }
-
-  // METHODS
-  // Insert element
-  enqueue(element) {
-    this.elements[this.count] = element;
-    this.count++;
-  }
-
-  // Removes an element
-  dequeue() {
-    if (this.isEmpty()) {
-      return undefined;
+    constructor() {
+        this.count = 0;
+        this.lowestCount = 0;
+        this.elements = {};
     }
 
-    const result = this.elements[this.lowestCount];
-    delete this.elements[this.lowestCount];
-    this.lowestCount++;
-    return result;
-  }
-
-  // Convert the queue to string
-  toString() {
-    let arr = [];
-
-    for (let i = this.lowestCount; i < this.count; i++) {
-      arr.push(this.elements[i]);
+    // METHODS
+    // Insert element
+    enqueue(element) {
+        this.elements[this.count] = element;
+        this.count++;
     }
-    return arr.toString();
-  }
 
-  // Return if the queue it's empty
-  isEmpty() {
-    return this.size() === 0;
-  }
+    // Removes an element
+    dequeue() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
 
-  // Return the size of the queue
-  size() {
-    return this.count - this.lowestCount;
-  }
+        const result = this.elements[this.lowestCount];
+        delete this.elements[this.lowestCount];
+        this.lowestCount++;
+        return result;
+    }
+
+    // Convert the queue to string
+    toString() {
+        let arr = [];
+
+        for (let i = this.lowestCount; i < this.count; i++) {
+            arr.push(this.elements[i]);
+        }
+        return arr.toString();
+    }
+
+    // Return if the queue it's empty
+    isEmpty() {
+        return this.size() === 0;
+    }
+
+    // Return the size of the queue
+    size() {
+        return this.count - this.lowestCount;
+    }
 }
 
 // FUNCTIONS
 // Remove from queue, according to call rule
 function remove() {
-  if (priorityCount < 2 && !priorityQueue.isEmpty()) {
-    priorityQueue.dequeue();
-    priorityCount += 1;
-  } else if (commonQueue.isEmpty() && !priorityQueue.isEmpty()) {
-    priorityQueue.dequeue();
-    priorityCount += 1;
-  } else if (!commonQueue.isEmpty()) {
-    commonQueue.dequeue();
-    priorityCount = 0;
-  }
-  updateDisplay();
+    if (priorityCount < 2 && !priorityQueue.isEmpty()) {
+        priorityQueue.dequeue();
+        priorityCount += 1;
+    } else if (commonQueue.isEmpty() && !priorityQueue.isEmpty()) {
+        priorityQueue.dequeue();
+        priorityCount += 1;
+    } else if (!commonQueue.isEmpty()) {
+        commonQueue.dequeue();
+        priorityCount = 0;
+    }
+    updateDisplay();
 }
 
 // Insert element on the Common Queue
 function insertCommom() {
-  if (commonQueue.isEmpty()) commonElement = 1;
-  commonQueue.enqueue(commonElement);
-  commonElement++;
-  updateDisplay();
+    if (commonQueue.isEmpty()) commonElement = 1;
+    commonQueue.enqueue(commonElement);
+    commonElement++;
+    updateDisplay();
 }
 
 // Insert element on the Priority Queue
 function insertPriority() {
-  if (priorityQueue.isEmpty()) priorityElement = 1;
-  priorityQueue.enqueue(priorityElement);
-  priorityElement++;
-  updateDisplay();
+    if (priorityQueue.isEmpty()) priorityElement = 1;
+    priorityQueue.enqueue(priorityElement);
+    priorityElement++;
+    updateDisplay();
 }
 
 // Update the values shown of the Queues
 function updateDisplay() {
-  let priority = document.getElementById("PriorityQueueID");
-  let common = document.getElementById("CommonQueueID");
-  priority.innerHTML = `FILA PRIORITÁRIA: [${priorityQueue.toString()}]`;
-  common.innerHTML = `FILA COMUM: [${commonQueue.toString()}]`;
+    let priority = document.getElementById("PriorityQueueID");
+    let common = document.getElementById("CommonQueueID");
+    priority.innerHTML = `FILA PRIORITÁRIA: [${priorityQueue.toString()}]`;
+    common.innerHTML = `FILA COMUM: [${commonQueue.toString()}]`;
 }
 
 // MAIN
